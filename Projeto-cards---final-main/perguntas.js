ce70499535288ca8d422a6c8bf34f16e4699e48e
Samuel Marçal <161322021+samueldoscartaoclonado@users.noreply.github.com>
2024-11-04 08:08:22 -0300

    Add files via upload

---
+++ Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Historia',
+    'Quando foi a Independência do Brasil?',
+    'A Independência do Brasil foi em 7 de Setembro de 1822'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da frança?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+criaCartao(
+    'Futebol',
+    'Quando foi fundado o Corinthians?',
+    'Foi fundado em 1 de Setembro de 1910'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'Historia',
+    'Quando começou a Republica?',
+    'Em 15 de Novembro de 1889'
+)
+
+criaCartao(
+    'Historia',
+    'Quando foi a Ida a Lua?',
+    'Em 16 de julho de 1969'
+)
